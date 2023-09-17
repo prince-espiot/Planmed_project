66ee598af3f0ab831ad7de4cac8f5f38d24ef490
okumo <prince.okumo@nokia.com>
2023-09-17 22:44:44 +0300

    adding test case file with template to execute the unit testing

---
+++ test.cpp
@@ -0,0 +1,55 @@
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
+#include "gtest/gtest.h"
+#include "dose.h"
+#include "types.h"
+#include "parameters.h"
+#include "tables.h"
+#include "dose.c"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+//In the next three test cases, I am testing my self_created functions 
+//created with the aim of refactoring the assignment.
+TEST(CalcInterpolateTest, TestwithValidInputs) {
+    // Set up input values
+    u8 p = 5;
+    u8 limited_mm = 30;
+    u8 mm_index[9] = { 25 }; 
+    u32 result = calcInterpolate(p, limited_mm, mm_index);
+    // Assert the result
+    EXPECT_EQ(result, (1000 * (limited_mm - mm_index[p])) / (mm_index[p + 1] - mm_index[p]));
+}
+
+TEST(CalcInterpolateTest, TestWithInvalidInputPZero) {
+    // Set up input values that will lead to p being 0
+    u8 p = 0;
+    u8 limited_mm = 30;
+    u8 mm_index[1] = { 25 }; 
+    u32 result = calcInterpolate(p, limited_mm, mm_index);
+
+    // Assert the result
+    EXPECT_EQ(result, 0);
+}
+
+TEST(CalcMgdIedAnodeTest, TestWithValidInputs) { // I can also test for invalid inputs at were p=0 and kv =0
+    u8 kv = 80;
+    u8 p = 3;
+    u32 interpol = 2000;
+    u8 filter = 2;
+
+    u16 result = calcMgdIedAnode(MGD_Mo, kv, p, interpol);
+    u16 expected_result = MGD_Mo[p][kv - 20] - ((interpol * (MGD_Mo[p][kv - 20] - MGD_Mo[p + 1][kv - 20]) + 500) / 1000);
+
+    // Assert the result
+    EXPECT_EQ(result, expected_result);
+}
+
+// Now to test for calcIED
+//First test for valid input and test for invalid thickness since the thickness is alway confined to be between 10 and 100;
+TEST(CalcIEDAndMGDTest, CalcIED_ValidInputs) {
+    // Define valid inputs
+    // Call calcIED with these inputs
+    // Assert the result matches the expected IED value
+}
